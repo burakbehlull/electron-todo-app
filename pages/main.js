53389dd8e23f848dea29bbf9222ceaee5dd18bab
31000s <burakyabgu@gmail.com>
2023-04-24 01:39:45 +0300

    feat: Measuring todo total counter

--- pages/main.js
+++ pages/main.js
@@ -5,6 +5,7 @@ const addTodo = document.getElementById('addTodo')
 
 addTodo.addEventListener('click', ()=>{
     ipcRenderer.send('newTodo', todoValue.value)
+    todoValue.value = "";
 })
 
 ipcRenderer.on('todoItems', (err, todo)=>{
@@ -18,16 +19,21 @@ ipcRenderer.on('todoItems', (err, todo)=>{
     let deleteBtn = document.createElement('button')
     todoLi.appendChild(deleteBtn).innerHTML = "x"
     
+    counterFunc()
 
     deleteBtn.addEventListener('click', e=>{
-        if (confirm('Kayıdı silmek istediğinizden emin misinz?')){
+        if (confirm('Kayıdı silmek istediğinizden emin misiniz?')){
             e.target.parentNode.remove()
-            
+            counterFunc()
         }
     })
     
     
 })
 
+const counterFunc = () => {
+    let getCounter = document.getElementById('todoValue')
+    getCoutner.placeholder = `Toplam Todo: ${todos.children.length}`
+}
 
 
